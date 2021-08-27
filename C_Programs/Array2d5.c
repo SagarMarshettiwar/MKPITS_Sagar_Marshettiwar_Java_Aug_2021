9946e4a20052ae7b349e20211fb8c1a3d380af97
Sagar Marshettiwar <sagarmarshettiwar@gmail.com>
2021-08-27 22:42:31 +0530

    programs array2d

---
+++ C_Programs/Array2d5.c
@@ -0,0 +1,23 @@
+//2 create a 2d array of 2 rows and 4 columns and store some value in it and display the values
+int main()
+{
+	int arr[2][4];
+	int row,col;
+	for(row=0;row<2;row++)
+	{
+		for(col=0;col<4;col++) // row=2 and col=2
+		{
+			printf("enter number");
+			scanf("%d",&arr[row][col]) ;
+		}
+	}
+	//displaying the values of an array
+	for(row=0;row<2;row++)
+	{
+		for(col=0;col<4;col++)
+		{
+			printf("\t%d",arr[row][col]) ;
+		}
+	printf("\n");
+	}
+}
