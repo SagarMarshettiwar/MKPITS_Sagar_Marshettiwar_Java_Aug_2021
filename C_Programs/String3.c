5e134fe3ceb39637e555934cb8880f3ecc60c133
Sagar Marshettiwar <sagarmarshettiwar@gmail.com>
2021-08-31 19:59:45 +0530

    string and string function

---
+++ C_Programs/String3.c
@@ -0,0 +1,5 @@
+//4-wap to create a string using string literal.
+int main() {
+	char ch[]="amitabh" ;
+	printf("\n name =%s",ch);
+}
---
+++ C_Programs/StringFunction6.c
@@ -0,0 +1,12 @@
+//18 -strlwr() function
+//wap to convert string into lower case
+
+#include<string.h>
+int main()
+{
+	char name[30];
+	printf("enter firstname");
+	gets(name);
+	strlwr(name);
+	printf("\n name = %s",name);
+}
