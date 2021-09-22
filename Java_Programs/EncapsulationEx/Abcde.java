f0ea94b0421c5924ec29f832ea98d41012cacc80
Sagar Marshettiwar <sagarmarshettiwar@gmail.com>
2021-09-22 23:05:47 +0530

    Encapsulation using Package

---
+++ Java_Programs/EncapsulationEx/Abcde.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EncapsulationEx;
+import EncapsulationExample.Children;
+/**
+ *
+ * @author SAGAR
+ */
+public class Abcde {
+    public static void main(String[] args) {
+        Children c=new Children();
+        c.setName("Sagar");
+        System.out.println("Name ="+c.getName());
+    }
+}
