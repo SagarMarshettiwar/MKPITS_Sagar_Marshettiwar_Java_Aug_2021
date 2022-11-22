4b7cd183738ac8bf29f22630617bc64231209449
unknown <TRUST@COM156.tssiplngp.ad>
2022-11-22 12:13:45 +0530

    spring core

---
+++ Spring Tutorial/springcore/src/main/java/com/springcore/autowire/annotation/Employee.java
@@ -0,0 +1,39 @@
+package com.springcore.autowire.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Employee {
+	@Autowired
+	@Qualifier("add1")
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	/* @Autowired */
+	public void setAddress(Address address) {
+		System.out.println("setter method");
+		this.address = address;
+	}
+
+	/* @Autowired */
+	public Employee(Address address) {
+		super();
+		this.address = address;
+		System.out.println("inside constructor");
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+	
+	
+}
